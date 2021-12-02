b8f88c71a82a6dc5bf7a5e4b714af499d1238aa7
Nicola-Perera <nicola.perera96@mail.com>
2021-12-02 12:05:13 +0100

    add loop for to show on screen the "matches22 array elements

--- index.php
+++ index.php
@@ -33,3 +33,19 @@ $matches = array(
 
 var_dump($matches);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snacks-b1</title>
+</head>
+<body>
+
+<p><?php
+    // for ($i = 0; $i < count($matches); $i++) {$matches[$i]};
+?></p>
+</body>
+</html>
