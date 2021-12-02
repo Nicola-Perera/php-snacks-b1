eb2bb79e22ce60751f8fedb8c7355b9c2477ebe3
Nicola-Perera <nicola.perera96@mail.com>
2021-12-02 11:42:35 +0100

    add array "matches" with 1st match as 1st element

--- index.php
+++ index.php
@@ -7,5 +7,10 @@ Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
 -->
 <?php
+$matches = array('partita1' => array('squadra1' => 'Olimpia Milano',
+    'squadra2' => 'Milano',
+    'Punteggio squadra1' => '55',
+    'Punteggio squadra2' => '60'));
 
+var_dump($matches);
 ?>
